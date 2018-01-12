04932977401b351d4d92fce5f9ce7383200c8482
Dario Ghirardo <dario.ghirardo@gmail.com>
2018-01-12 16:00:03 +0100

    Minor edits

--- scraper.js
+++ scraper.js
@@ -2,38 +2,61 @@ const Xray = require('x-ray');
 const x = Xray();
 const fs = require('fs');
 const csv = require('fast-csv');
+const fields = ['Title', 'Price', 'ImgURL', 'URL', 'Time'];
 
 // search for 'data' folder, if doesn't exists then create it
-try {
-  fs.statSync('./data');
-} catch(e) {
-  fs.mkdirSync('./data');
+function checkFolder () {
+  try {
+    fs.statSync('./data');
+  } catch(e) {
+    fs.mkdirSync('./data');
+  }
 }
 
-// generate new date in format year-month-day
-const dt = new Date();
-const date = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate();
-// send a write stream to the new csv files named after current date
-const ws = fs.createWriteStream(`./data/${date}.csv`);
+// go to website, scrape content and save it
+function scrape () {
+  checkFolder();
 
-// scraping function that uses 'x-ray' module
-x('http://www.shirts4mike.com/shirt.php', '.products li', [{
-  Title: x("a@href", ".shirt-details h1"),
-  Price: x("a@href", ".price"),
-  ImgURL: x("a@href", "img@src"),
-  URL: "a@href",
-}])((err, obj) => {
-  // if error then throw message and save it in 'scraper-error.log'
-  if (err) {
-    console.log(`There’s been an error. Cannot connect to http://shirts4mike.com. (${err.code})`);
-    fs.appendFileSync('./data/scraper-error.log', `[${dt}] ${err.message} \n`, encoding='utf8');
-    return
-  }
-  // assign 'Time' value and adjust 'Title' value
-  for (let i = 0; i < 8; i++) {
-    obj[i].Time = new Date();
-    obj[i].Title = obj[i].Title.slice(4);
-  }
-  // convert scraped content to csv using 'fast-csv' module and then send stream to csv file
-  csv.write(obj,{headers:true}).pipe(ws);
-})
+  // generate new date in format year-month-day
+  const dt = new Date();
+  const date = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate();
+  // send a write stream to the new csv files named after current date
+  const ws = fs.createWriteStream(`./data/${date}.csv`);
+
+  // scrape and crawl function that uses 'x-ray' module
+  x('http://www.shirts4mike.com/shirt.php', '.products li', [{
+    Title: x("a@href", ".shirt-details h1"),
+    Price: x("a@href", ".price"),
+    ImgURL: x("a@href", "img@src"),
+    URL: "a@href",
+  }])
+  // callback function
+  ((err, obj) => {
+    // if error occurs create error message
+    if (err) {
+      const errorMessage = `There’s been an error. Cannot connect to ${err.hostname}. (errorCode: ${err.code})`
+      // show message
+      console.error(errorMessage);
+      // save error in a new line of 'scraper-error.log'
+      fs.appendFileSync('./data/scraper-error.log', `[${dt}] ${errorMessage} \r\n`, encoding='utf8');
+      return
+    }
+    // assign 'Time' value and adjust 'Title' value
+    for (let i = 0; i < 8; i++) {
+      obj[i].Time = new Date();
+      obj[i].Title = obj[i].Title.slice(4);
+    }
+    // use 'fast-csv' module to convert scraped content to csv
+    csv
+      .write(obj,{
+        // with 'fields' array as columns
+        headers: fields
+      })
+      // send stream to csv file
+      .pipe(ws);
+      // show success message
+    console.log('Content saved.')
+  })
+}
+
+scrape();
