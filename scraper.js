f3fe9fa7656387e20bfdd3708bac1cc4c0e49776
Ghir <33903713+Ghir@users.noreply.github.com>
2019-03-25 22:09:05 +0100

    Edited comments

--- scraper.js
+++ scraper.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const csv = require('fast-csv');
 const fields = ['Title', 'Price', 'ImgURL', 'URL', 'Time'];
 
-// search for 'data' folder, if doesn't exists then create it
+// search or create data folder
 function checkFolder () {
   try {
     fs.statSync('./data');
@@ -13,13 +13,11 @@ function checkFolder () {
   }
 }
 
-// go to website, scrape content and save it
 function scrape () {
   checkFolder();
-  // generate new date in format year-month-day
   const dateTime = new Date();
   const date = dateTime.getFullYear() + "-" + (dateTime.getMonth() + 1) + "-" + dateTime.getDate();
-  // send a write stream to the new csv file named after current date
+  // send a write stream to the new csv file
   const writeStream = fs.createWriteStream(`./data/${date}.csv`);
 
   // scrape and crawl function that uses 'x-ray' module
@@ -29,31 +27,23 @@ function scrape () {
     ImgURL: x("a@href", "img@src"),
     URL: "a@href",
   }])
-  // callback
   ((err, obj) => {
-    // if error occurs create error message
     if (err) {
       const errorMessage = `There’s been an error. Cannot connect to ${err.hostname}. (errorCode: ${err.code})`
-      // show message
       console.error(errorMessage);
-      // save error in a new line of 'scraper-error.log'
       fs.appendFileSync('./data/scraper-error.log', `[${dateTime}] ${errorMessage} \r\n`, encoding='utf8');
       return
     }
-    // assign 'Time' value and adjust 'Title' value
     for (let i = 0; i < 8; i++) {
       obj[i].Time = new Date();
       obj[i].Title = obj[i].Title.slice(4);
     }
-    // use 'fast-csv' module to convert scraped content to csv
+    // 'fast-csv' module to convert scraped content to csv
     csv
       .write(obj,{
-        // with 'fields' array as columns
         headers: fields
       })
-      // send stream to csv file
       .pipe(writeStream);
-      // show success message
     console.log('Content saved.')
   })
 }
